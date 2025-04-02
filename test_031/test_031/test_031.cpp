edb654e8bf9306d4bf5860476cf40f9d74e81987
Lazylionr6 <syeop98@gmail.com>
2025-04-02 15:10:05 +0900

    text_appearence_order

--- test_031/test_031/test_031.cpp
+++ test_031/test_031/test_031.cpp
@@ -1,12 +1,51 @@
 ﻿// test_031.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
 
-#include <iostream>
-
-int main()
+/*int main(void)
 {
-    std::cout << "Hello World!\n";
+	char alpha;
+
+	scanf("%c", &alpha);
+
+	printf("%d", alpha);
+
+	return 0;
+}*/
+
+int main(void)
+{
+	char S[100];
+	int Alpha[26];				//알파벳 개수 : 26개 (65 ~ 90)
+	int i, l, n;				//l: 문자의 길이  n: 알파벳 순서
+
+
+	scanf("%s", &S);			//문자 대입
+
+	l = strlen(S);				//문자 길이
+
+	for (i = 0; i < 26;i++) {	//배열 초기값 조정
+		Alpha[i] = -1;
+	}
+
+	for (i = 0;i < l;i++) {
+		n = S[i] - 'a';			//알바벳 순서 맞추기
+
+		if (Alpha[n] == -1) {
+			Alpha[n] = i;			//등장 순서 조정
+		}
+	}
+
+	for (i = 0;i < 26;i++) {
+		printf("%d ", Alpha[i]);
+	}
+	return 0;
 }
+
+
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
 // 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
